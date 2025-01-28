24aff6407b8ecbf326745a1f67306a2518621f60
Jonathan Perez <thejonathan38a10@gmail.com>
2025-01-28 21:24:23 +0100

    resolve merge conflict

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -1,8 +1,16 @@
-package controllers;
+package org.example.mibocatafx;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class LoginController {
     @FXML
@@ -20,9 +28,28 @@ public class LoginController {
         String userPassword = passField.getText();
 
         if (userMail.equals(validUserMail) && userPassword.equals(validUserPassword)) {
-            System.out.println("Loged");
+            try {
+                abrirVentana();
+            } catch (IOException e) {
+                System.out.println("Error al entrar");
+            }
         } else {
             System.exit(-1);
         }
+    }
+
+    @FXML
+    protected void abrirVentana() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("principal.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        Stage stage = new Stage();
+        stage.setTitle("Selección Bocadillo");
+        stage.setMaximized(true);
+        stage.setScene(scene);
+        stage.show();
+
+        Stage anteriorVentana = (Stage) userInput.getScene().getWindow();
+        anteriorVentana.close();
     }
 }
