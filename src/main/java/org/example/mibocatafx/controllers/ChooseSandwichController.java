c6178b375883f034722ac8459a211d97a7fd5679
Jonathan Perez <thejonathan38a10@gmail.com>
2025-02-08 00:34:59 +0100

    add chooseSandwichScreen.fxml design

--- src/main/java/org/example/mibocatafx/controllers/ChooseSandwichController.java
+++ src/main/java/org/example/mibocatafx/controllers/ChooseSandwichController.java
@@ -2,14 +2,22 @@ package org.example.mibocatafx.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 
 public class ChooseSandwichController {
     @FXML
     private Pane backgroundPane;
 
+    @FXML
+    private Button orderButton;
+
     public void initialize() {
         backgroundPane.getStylesheets().add(getClass().getResource("/css/chooseSandwichStyle.css").toExternalForm());
+
+        orderButton.setOnMouseEntered(mouseEvent -> {
+            orderButton.setStyle("-fx-background-color: #0080D0");
+        });
 
     }
 }
