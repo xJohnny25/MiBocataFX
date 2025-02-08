d36fac0df56e21c3e52eadc0064725c7a1f2a0cb
Jonathan Perez <thejonathan38a10@gmail.com>
2025-02-08 15:23:07 +0100

    add hibernate encapsulated connection

--- src/main/java/org/example/mibocatafx/util/HibernateConnection.java
+++ src/main/java/org/example/mibocatafx/util/HibernateConnection.java
@@ -1,29 +1,22 @@
 package org.example.mibocatafx.util;
 
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateConnection {
-//    private static SessionFactory sessionFactory;
-//
-//    static {
-//        try {
-//            sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
-//        } catch (Throwable ex) {
-//            System.err.println("Error al inicializar SessionFactory: " + ex);
-//            throw new ExceptionInInitializerError(ex);
-//        }
-//    }
-//
-//    public static Session getSession() {
-//        return sessionFactory.openSession();  // Devuelve una nueva sesión
-//    }
-//
-//    public static void close() {
-//        if (sessionFactory != null) {
-//            sessionFactory.close();
-//        }
-//    }
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            return new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Error al crear SessionFactory: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
 }
 
