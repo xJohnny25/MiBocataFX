199f54903e3c709d4c7f8ceeb99e601f4189b4dd
Jonathan Perez <thejonathan38a10@gmail.com>
2025-02-13 03:00:14 +0100

    fix project files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,8 +6,17 @@ module org.example.mibocatafx {
     requires java.naming;
 
 
-    opens org.example.mibocatafx to javafx.fxml;
+    opens org.example.mibocatafx to javafx.fxml, org.hibernate.orm.core;
     exports org.example.mibocatafx;
+    exports org.example.mibocatafx.dao;
+    opens org.example.mibocatafx.dao to javafx.fxml, org.hibernate.orm.core;
     exports org.example.mibocatafx.controllers;
-    opens org.example.mibocatafx.controllers to javafx.fxml;
+    opens org.example.mibocatafx.controllers to javafx.fxml, org.hibernate.orm.core;
+    exports org.example.mibocatafx.util;
+    opens org.example.mibocatafx.util to javafx.fxml, org.hibernate.orm.core;
+    exports org.example.mibocatafx.models;
+    opens org.example.mibocatafx.models to javafx.fxml, org.hibernate.orm.core;
+    exports org.example.mibocatafx.service;
+    opens org.example.mibocatafx.service to javafx.fxml, org.hibernate.orm.core;
+
 }
