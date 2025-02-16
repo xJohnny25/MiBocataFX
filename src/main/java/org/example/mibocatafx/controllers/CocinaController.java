da33453752eaa7cfb01465e4853b97ab226abdb0
xJohnny25 <Thejonathan38a10@gmail.com>
2025-02-17 00:38:52 +0100

    add cocina design and initialize fxml objects

--- src/main/java/org/example/mibocatafx/controllers/CocinaController.java
+++ src/main/java/org/example/mibocatafx/controllers/CocinaController.java
@@ -1,4 +1,45 @@
 package org.example.mibocatafx.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+
 public class CocinaController {
+    @FXML
+    private HBox logOutButton;
+
+    @FXML
+    private TextField nameInput;
+
+    @FXML
+    private TextField lastNameInput;
+
+    @FXML
+    private ComboBox<String> cursesBox;
+
+    @FXML
+    private DatePicker calendarInput;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private TableView table;
+
+    @FXML
+    private ComboBox numPages;
+
+    @FXML
+    private Button previusPage;
+
+    @FXML
+    private Button nextPage;
+
+    @FXML
+    private Label pagesCount;
+
+    @FXML
+    private Label resultsCount;
+
+
 }
